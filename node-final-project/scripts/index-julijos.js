fd25e50501c6b6389c34c9334364bc960b131df5
Ju-chan <75089351+Ju-chan@users.noreply.github.com>
2021-02-28 18:53:11 +0200

    4

--- node-final-project/scripts/index-julijos.js
+++ node-final-project/scripts/index-julijos.js
@@ -1,11 +1,17 @@
 // Variables
 const signInButton = document.querySelector(".sign-in");
-const signInForm = document.querySelector(".signin-options");
+const closeBtn = document.querySelector(".closebtn");
 
 // Function
 function openSignIn() {
-  signInForm.style.display = "block";
-  signInForm.style.height = "100vh";
+  document.getElementById("mySignIn").style.height = "100%";
+  document.getElementById("mySignIn").style.width = "50%";
+  document.getElementById("mySignIn").style.justifyContent = "center";
+}
+function closeSignIn() {
+  document.getElementById("mySignIn").style.height = "0%";
 }
 
 // Events
+signInButton.addEventListener("click", () => openSignIn);
+closeBtn.addEventListener("click", () => closeSignIn);
