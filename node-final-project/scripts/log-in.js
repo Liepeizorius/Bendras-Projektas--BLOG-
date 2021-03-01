00acace68adf19d74391dfc80c3a4ea1fe72e394
Ju-chan <75089351+Ju-chan@users.noreply.github.com>
2021-03-01 21:22:12 +0200

    pirm8

--- node-final-project/scripts/log-in.js
+++ node-final-project/scripts/log-in.js
@@ -1,35 +1,37 @@
-window.onload = function() {
-    document.querySelector('#logSubmit').addEventListener('click', async (e) => {
-        e.preventDefault();
-        let username = document.querySelector('#username').value;
-        let password = document.querySelector('#password').value;
+window.onload = function () {
+  document.querySelector("#logSubmit").addEventListener("click", async (e) => {
+    e.preventDefault();
+    let username = document.querySelector("#username").value;
+    let password = document.querySelector("#password").value;
 
-        let data = {
-            username,
-            password
+    let data = {
+      username,
+      password,
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:1111/api/v1/scripts/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+
+          body: JSON.stringify(data),
         }
+      );
 
-        try {
-            const response = await fetch('http://localhost:1111/api/v1/scripts/login', {
-                method: 'POST',
-                headers: {
-                    "Content-Type": 'application/json'
-                },
-    
-                body: JSON.stringify(data)
-            })
-            
-            if (response.status != 200) throw await response.json()
+      if (response.status != 200) throw await response.json();
 
-            let token = response.headers.get('user-auth')
-            localStorage.setItem('user-auth', token)
-    
-    
-            console.log(await response.json())
-        
-        } catch(e) {
-            alert(e)
-        }
+      let token = response.headers.get("user-auth");
+      localStorage.setItem("user-auth", token);
 
-    })
-  }
+      console.log(await response.json());
+      // Perregistruoja į user-write.html
+      window.location.href = "../pages/user-write.html";
+    } catch (e) {
+      alert(e);
+    }
+  });
+};
