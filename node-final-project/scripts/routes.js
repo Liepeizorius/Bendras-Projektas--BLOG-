87dfbb9fc67a1b0bf030d6d7ef7087b32d2cc9b7
Ju-chan <75089351+Ju-chan@users.noreply.github.com>
2021-03-02 16:44:33 +0200

    User-write turinys, upload image funkcija, į kitus puslapius įdėta font-awesome linkas

--- node-final-project/scripts/routes.js
+++ node-final-project/scripts/routes.js
@@ -1,18 +1,32 @@
-const router = require('express').Router();
-const UserController = require('./userController');
-const UserMiddleware = require('./authenticate');
+const router = require("express").Router();
+const UserController = require("./userController");
+const UserMiddleware = require("./authenticate");
 
-router.get('/', (request, response) => {
-  response.json('Hello world!');
+// Padeda apdoroti failus, ateinančius į serverį
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+const upload = multer({
+  storage,
 });
 
-router.post('/scripts/signup', UserController.signUp);
-router.post('/scripts/login', UserController.login);
+router.get("/", (request, response) => {
+  response.json("Hello world!");
+});
+
+router.post("/scripts/signup", UserController.signUp);
+router.post("/scripts/login", UserController.login);
 router.get(
-  '/scripts/logout',
+  "/scripts/logout",
   UserMiddleware.authenticate,
   UserController.logout
 );
-router.get('/scripts', UserMiddleware.authenticate, UserController.getAllUsers);
+router.get("/scripts", UserMiddleware.authenticate, UserController.getAllUsers);
 
 module.exports = router;
