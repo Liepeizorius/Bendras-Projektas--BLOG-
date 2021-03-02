5e08ffc1812f92318bb4bee87032d77334406243
Gediminas <66636836+Liepeizorius@users.noreply.github.com>
2021-03-02 20:42:54 +0200

    Refactoring ar kazkas tai

---
+++ node-final-project/scripts/Back-end/routes/routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const UserController = require('../userController');
+const UserMiddleware = require('../authenticate');
+
+// Padeda apdoroti failus, ateinančius į serverį
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+const upload = multer({
+  storage,
+});
+
+router.get('/', (request, response) => {
+  response.json('Hello world!');
+});
+
+router.post('/scripts/Back-end/signup', UserController.signUp);
+router.post('/scripts/Back-end/login', UserController.login);
+router.get(
+  '/scripts/Back-end/logout',
+  UserMiddleware.authenticate,
+  UserController.logout
+);
+router.get('/scripts', UserMiddleware.authenticate, UserController.getAllUsers);
+
+module.exports = router;
---
+++ node-final-project/scripts/Front-end/log-in.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+  document.querySelector('#logSubmit').addEventListener('click', async (e) => {
+    e.preventDefault();
+    let username = document.querySelector('#username').value;
+    let password = document.querySelector('#password').value;
+
+    let data = {
+      username,
+      password,
+    };
+
+    try {
+      const response = await fetch(
+        'http://localhost:1111/api/v1/scripts/Back-end/login',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (response.status != 200) throw await response.json();
+
+      let token = response.headers.get('user-auth');
+      localStorage.setItem('user-auth', token);
+
+      console.log(await response.json());
+      // Perregistruoja į user-write.html
+      window.location.href = '../pages/user-write.html';
+    } catch (e) {
+      alert(e);
+    }
+  });
+};
